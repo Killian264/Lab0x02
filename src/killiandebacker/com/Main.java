bb298d52f01c0e68138124bf9186a6544cb83d73
Killian264 <killiandebacker@gmail.com>
2020-09-29 20:29:59 -0500

    final version

--- src/killiandebacker/com/Main.java
+++ src/killiandebacker/com/Main.java
@@ -11,13 +11,75 @@ public class Main {
 
         // user must cancel
 
+        int keyStart = 16;
+        int countStart = 100;
+
+        String[] testStrings = {"Selection", "Insertion", "Radix", "QuickSort", "MergeSort"};
+
+        for(int i = 0; i < testStrings.length; i++){
+            int key = keyStart;
+            int count = countStart;
+            System.out.println("key = " + key);
+            System.out.println("sort type = " + testStrings[i]);
+            padder("N"); padder("Time"); padder("Doubling Ratio");
+            System.out.println();
+
+            long prevTimeTaken = 1;
+            while(true){
+                padder(Integer.toString(count));
+                prevTimeTaken = Run(count, key, testStrings[i], prevTimeTaken);
+                prevTimeTaken = prevTimeTaken >  0 ? prevTimeTaken : 1;
+                if(prevTimeTaken > 10000000000L){
+                    break;
+                }
+                count *= 2;
+            }
+        }
+    }
+
+    private static long Run(int count, int key, String sortType, long prev){
+        String[] arr =  GenerateTestList(count, key, false);
+
+        long timeStampBefore = getCpuTime();
+
+        if(sortType == "Insertion"){
+            InsertionSortList(arr);
+        }
+        else if(sortType == "Selection"){
+            SelectionSortList(arr);
+        }
+        else if(sortType == "Radix"){
+            RadixSortList(arr, key);
+        }
+        else if(sortType == "QuickSort"){
+            StringQuickSort sorter = new StringQuickSort();
+            arr = sorter.sort(arr);
+        }
+        else if(sortType == "MergeSort"){
+            MergeSort sorter = new MergeSort();
+            sorter.mergeSort(arr, 0, arr.length - 1);
+        }
+
+
+        long timeStampAfter = getCpuTime();
+        long time = timeStampAfter - timeStampBefore;
+
+        double actual = (time / prev);
+
+        numberPadder(time);
+        padder(Double.toString(actual) + "x");
+        System.out.println();
+
+        return time;
+    }
+
+    // Testing you can ignore this
+    private static void doTests(){
         int key = 16;
         int count = 100;
-
         while(true){
             System.out.println("Generating list of length " + count  + ", key width of " + key + ": ");
-            char[][] arr =  GenerateTestList(count, key, true);
-
+            String[] arr =  GenerateTestList(count, key, true);
             System.out.println("Sorting with Selection Sort");
             SelectionSortList(arr);
             System.out.print("Verifying...  ");
@@ -32,7 +94,44 @@ public class Main {
 
             System.out.println("Sorting with Insertion Sort");
             InsertionSortList(arr);
-            System.out.println("Verifying...");
+            System.out.print("Verifying...  ");
+            if(IsSorted(arr)){
+                System.out.println("Sorted");
+            }
+            else{
+                System.out.println("ERROR NOT SORTED");
+            }
+
+            arr =  GenerateTestList(count, key, true);
+
+            System.out.println("Sorting with Radix Sort");
+            arr = RadixSortList(arr, key);
+            System.out.print("Verifying...  ");
+            if(IsSorted(arr)){
+                System.out.println("Sorted");
+            }
+            else{
+                System.out.println("ERROR NOT SORTED");
+            }
+
+            arr =  GenerateTestList(count, key, true);
+
+            System.out.println("Sorting with Quick Sort");
+            StringQuickSort sorter = new StringQuickSort();
+            arr = sorter.sort(arr);
+            System.out.print("Verifying...  ");
+            if(IsSorted(arr)){
+                System.out.println("Sorted");
+            }
+            else{
+                System.out.println("ERROR NOT SORTED");
+            }
+            arr =  GenerateTestList(count, key, true);
+
+            System.out.println("Sorting with Quick Sort");
+            MergeSort sorter2 = new MergeSort();
+            sorter2.mergeSort(arr, 0, arr.length - 1);
+            System.out.print("Verifying...  ");
             if(IsSorted(arr)){
                 System.out.println("Sorted");
             }
@@ -40,53 +139,28 @@ public class Main {
                 System.out.println("ERROR NOT SORTED");
             }
             count *= 2;
-
         }
     }
-
-    private static void RunTests(){
-
-//        System.out
-//        // INSERTION ASC
-//        char[] arr =  GenerateTestList(100, 10, true);
-//
-//        for(int )
-//
-//        InsertionSort(arr, arr.length);
-//
-//        printArray(arr);
-//
-//
-//        // SELECTION DESC
-//        arr =  GenerateTestList(100, 10, true);
-//
-//
-//        SelectionSort(arr, arr.length);
-//
-//        printArray(arr);
-    }
-
-
-    private static void printArray(char[] arr){
-        System.out.print("{ ");
-        for(char num : arr) {
-            System.out.print(num + ", ");
+    //print array
+    private static void printArray(String[] arr){
+        System.out.println();
+        for(String str : arr){
+            System.out.print(str + ", ");
         }
-        System.out.print("}");
         System.out.println();
     }
 
     // ARRAY GENERATORS
-    private static char[][] GenerateTestList(int N, int k, boolean useAlphabet){
-        char[][] strArr = new char[N][k];
-        for (int i = 0; i < k; i++) {
+    private static String[] GenerateTestList(int N, int k, boolean useAlphabet){
+        String[] strArr = new String[N];
+        for (int i = 0; i < N; i++) {
             strArr[i] = GenerateTestString(k, useAlphabet);
         }
         return strArr;
     }
 
 
-    private static char[] GenerateTestString(int k, boolean useAlphabet){
+    private static String GenerateTestString(int k, boolean useAlphabet){
         char[] arr = new char[k];
         Random r = new Random();
         if(useAlphabet) {
@@ -96,57 +170,77 @@ public class Main {
             }
         }
         else{
+            // for every character up to k generate a random int up to 255 and cast it to a character
             for (int i = 0; i < k; i++) {
                 arr[i] = (char)(r.nextInt(255) + 1);
             }
         }
-        return arr;
+        String yee = new String(arr);
+//        System.out.println(yee);
+        return yee;
     }
 
     // SORT VALIDATION
-    private static boolean IsSorted(char[][] arr){
-        for(char[] str : arr){
-            if(!IsSortedString(str) ){
+    private static boolean IsSorted(String[] arr){
+        String prev = arr[0];
+        // for each string check if this string is greater than the last
+        for(String str : arr){
+            if(prev.compareTo(str) > 0){
                 return false;
             }
+            prev = str;
         }
         return true;
     }
 
-    private static boolean IsSortedString(char[] arr){
-        for(int i = 1; i < arr.length; i++){
-            if(arr[i] > arr[i-1]){
-                return false;
+    private static String[] RadixSortList(String[] arr, int k){
+        int[] bucket = new int[256];
+        int[] prefixSum = new int[256];
+
+        // for each char in strings
+        // this is as alphabetical order goes from most important to least important like aa > abb > abc
+        for(int i = k-1; i >= 0; i--){
+            // I could also just swap arr and output
+            String[] output = new String[arr.length];
+            // for each string in array
+            for(int j = 0; j < arr.length; j++){
+                // add one to the bucket if the string goes in the bucket
+                bucket[(int)arr[j].charAt(i)] += 1;
             }
+            // generate prefixSum
+            int sum = 0;
+            for(int j = 0; j < bucket.length; j++){
+                prefixSum[j] = bucket[j] + sum;
+                sum = prefixSum[j];
+                bucket[j] = 0;
+            }
+
+            // build output using prefixSum
+            for(int j = arr.length - 1; j >= 0; j--){
+                int bucket_loc = (int)arr[j].charAt(i);
+                prefixSum[bucket_loc] -= 1;
+                output[prefixSum[bucket_loc]] = arr[j];
+            }
+            // swap
+            arr = output;
         }
-        return true;
+        return arr;
     }
 
-    private static void InsertionSortList(char[][] arr){
-        for(char[] str : arr) {
-            if (!IsSortedString(str)) {
-                InsertionSort(str);
-            }
-        }
-    }
-
-    // SORTS
-    private static void InsertionSort(char[] arr){
+    private static void InsertionSortList(String[] arr){
 
         // starting at element 1 as an array of element 1 will always be sorted
         // go through every element and ensure that 0 to i is sorted
         // if not move current element to its correct location
         for(int i = 1; i < arr.length; i++){
             // if previous greateer
-            if(arr[i] > arr[i - 1]){
+            if(arr[i].compareTo(arr[i - 1]) < 0){
                 // loop till current arr[j] less
                 for(int j = i - 1; j >= 0; j--){
-                    // if char is in correct location break
-                    if(arr[j] >= arr[j + 1]){
+                    if(arr[j].compareTo(arr[j + 1]) <= 0){
                         break;
                     }
-                    // swap
-                    char temp = arr[j];
+                    String temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
                 }
@@ -154,36 +248,62 @@ public class Main {
         }
     }
 
-    private static void SelectionSortList(char[][] arr){
-        for(char[] str : arr) {
-            if (!IsSortedString(str)) {
-                SelectionSort(str);
-            }
-        }
-    }
-
-    private static void SelectionSort(char[] arr){
-        char max;
+    private static void SelectionSortList(String[] arr){
+        String min;
         int loc;
-        char curr;
+        String curr;
 
         // for every element in the array
         for(int i = 0; i < arr.length; i++){
             loc = i;
-            max = arr[i];
+            min = arr[i];
             // for every element i to the length of the array
             for(int j = i; j < arr.length; j++){
                 // if the current is greater or less(depending on asc bool) set it to minmax
                 curr = arr[j];
-                if(curr > max){
-                    max = curr;
+                if(curr.compareTo(min) < 0){
+                    min = curr;
                     loc = j;
                 }
             }
             // swap minmax to its correct position at i
-            char temp = arr[i];
+            String temp = arr[i];
             arr[i] = arr[loc];
             arr[loc] = temp;
         }
+    }
+
+    private static void padder(String str){
+        int maxPadding = 20;
+        String padding = "";
+        for(int i = str.length(); i < maxPadding; i++){
+            padding += " ";
+        }
+        System.out.print("|" + str + padding + "|");
+    }
+
+    private static void numberPadder(long number){
+        String appended = "";
+        if(number < 8000000000000L && number > 8000000000L){
+            appended = "s";
+            number /= 1000000000;
+        }
+        else if(number < 8000000000L && number > 8000000){
+            appended = "ms";
+            number /= 1000000;
+        }
+        else if(number < 8000000 && number > 8000){
+            appended = "us";
+            number /= 1000;
+        }
+        else if(number < 8000){
+            appended = "ns";
+        }
+        padder(Long.toString(number) + appended);
+    }
+
+    public static long getCpuTime(){
+        ThreadMXBean bean = ManagementFactory.getThreadMXBean();
+        return bean.isCurrentThreadCpuTimeSupported() ? bean.getCurrentThreadCpuTime() : 0L;
     }
 }
